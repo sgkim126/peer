044032ccc36d335586981d58eb346ee293a787c9
Seulgi Kim <dev@seulgi.kim>
2026-06-04 10:37:45 +0900

    introduce ExtractError
    Each command will have its own error type to handle failures
    independently.

---
+++ src/extract/error.rs
@@ -0,0 +1,38 @@
+use std::fmt;
+
+use crate::git::GitError;
+
+#[derive(Debug)]
+#[expect(dead_code)]
+pub enum ExtractError {
+    Git(GitError),
+    InvalidTwoDotRange(String),
+}
+
+impl fmt::Display for ExtractError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Git(source) => {
+                write!(f, "cannot run git ({source})")
+            }
+            Self::InvalidTwoDotRange(range) => {
+                write!(f, "{range} is not a two-dot range")
+            }
+        }
+    }
+}
+
+impl std::error::Error for ExtractError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            Self::Git(source) => Some(source),
+            Self::InvalidTwoDotRange(_) => None,
+        }
+    }
+}
+
+impl From<GitError> for ExtractError {
+    fn from(err: GitError) -> Self {
+        Self::Git(err)
+    }
+}
